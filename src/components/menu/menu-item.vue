86a66b22633fbab78d8469a8aa1ca5343b7d43a0
Andreas Argelius <andreas@argeli.us>
2016-03-06 14:35:26 +0900

    Update menu

--- src/components/menu/menu-item.vue
+++ src/components/menu/menu-item.vue
@@ -4,6 +4,13 @@
   .menu__item
     padding 14px 16px
     font-size 17px
+    cursor pointer
+    user-select none
+    transition background-color 0.2s ease
+
+    &:active
+    &:hover
+      background-color $menu-item-active-color
 </style>
 
 <template>
